9fd5c37f04b951c67c76b0a6d9e4d334ec28b39c
SameerLags <sameerlags@gmail.com>
2019-05-11 20:29:46 +0530

    Project implemented

---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+function createGrid(gridSize=16){
+    const gridCont = document.querySelector(".gridContainer");
+    template = ""
+    for(i=0;i<gridSize;i++){
+        template += "1fr ";
+    }
+    gridCont.style["grid-template-columns"] = template;
+    gridCont.style["grid-template-rows"] = template;
+    for (i=0; i<gridSize**2; i++){
+        const gridItem = document.createElement("div");
+        gridItem.classList.add("gridItem");
+        gridItem.setAttribute("id", `${i+1}`);
+        gridCont.appendChild(gridItem);
+    };
+    mouseover();
+};
+createGrid();
+centreGrid();
+
+function centreGrid(){
+    let d = document.querySelector('.gridContainer');
+    let w = window.innerWidth;
+    let h = window.innerHeight;
+    let divW = d.clientWidth;
+    let divH = d.clientHeight;
+    d.style.position="absolute";
+    d.style.top = (h/2)-(divH/2)+"px";
+    d.style.left = (w/2)-(divW/2)+"px";
+};
+
+function resetGrid(e){
+    let gridSize = prompt("Enter squares per side");
+    const gridCont = document.querySelector(".gridContainer");
+    gridCont.innerHTML = "";
+    createGrid(gridSize);
+};
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", resetGrid);
+
+function mouseover(){
+    const gridItems = document.querySelectorAll(".gridItem");
+    gridItems.forEach(gridItem => {
+        gridItem.addEventListener('mouseover', (e) => {
+            e.target.classList.add("colorGridItem");
+        });
+    });
+};
